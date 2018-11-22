b94acfe37caabb5f33124b882ecaac70d9540377
Jian Han <jianhan@mail.com>
2018-11-22 21:39:54 +1000

    [feat] Download files finished

--- scraper.py
+++ scraper.py
@@ -1,13 +1,26 @@
 from bs4 import BeautifulSoup
 
 import requests
+import wget
+import os
+import shutil
 
 
 class Scraper:
 
     def __init__(self):
-        self.soup = BeautifulSoup(requests.get('http://www.football-data.co.uk/englandm.php').text, "lxml")
+        self.soup = BeautifulSoup(requests.get(
+            'http://www.football-data.co.uk/englandm.php').text, "lxml")
 
     def scrape(self):
+        dirName = 'epl_csvs'
+
+        if os.path.exists(dirName):
+            shutil.rmtree(dirName)
+
+        os.makedirs(dirName)
+
+        i = 0
         for link in self.soup.find_all('a', href=True, text='Premier League'):
-            print(link.get('href'))
+            wget.download('http://www.football-data.co.uk/' + link.get('href'), dirName+'/'+str(i)+'_'+'EPL.csv')
+            i = i + 1
