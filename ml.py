928db463d6c466dde142556575baaf9ab999aed5
Jian Han <jianhan@mail.com>
2018-11-24 23:49:07 +1000

    [feat] Drop columns wth more than half data missing

--- ml.py
+++ ml.py
@@ -15,6 +15,13 @@ class ML:
 
     def __description(self):
         df_length = len(self.df)
+        print(0.8 * df_length)
+        pass
+        for c in self.df.columns.values:
+            if df_length - self.df[c].count() > 0.5 * df_length:
+                # if the number of missing value is over 50%, then there is no point to leave
+                # this column in dataset
+                self.df.drop([c],axis=1,inplace=True)
 
         # table summary
         summary_table_data = [
@@ -28,6 +35,7 @@ class ML:
         columns_table_rows = [['column', 'type', 'missing values']]
         for c in self.df.columns.values:
             columns_table_rows.append([c, self.df.dtypes[c].name, df_length - self.df[c].count()])
+        
         columns_table = AsciiTable(columns_table_rows)
         print(columns_table.table)
 
@@ -35,9 +43,33 @@ class ML:
         # type casting
         self.df['date'] = pd.to_datetime(self.df.date)
 
+        # self.df.drop(
+        #     [
+        #         'away_team_bookings_points_10_yellow_25_red',
+        #         'away_team_hit_woodwork',
+        #         'away_team_offsides',
+        #         'crowd_attendance',
+        #         'bet365_under_2_5_goals',
+
+        #         'bet365_over_2_5_goals',
+
+        #         'bet365_away_win_odds',
+        #         'bet365_size_of_handicap_home_team',
+        #         'bet365_asian_handicap_away_team_odds',
+        #         'bet365_asian_handicap_home_team_odds',
+        #         'bet365_draw_odds',
+
+        #         'blue_square_away_win_odds',
+        #         'blue_square_draw_odds',
+        #         'blue_square_home_win_odds',
+        #         ],
+        #     axis=1,
+        #     inplace=True)
+        
+
     def __visualization(self):
         pass
-    
+
     def __feature_engineering(self):
         pass
 
@@ -49,10 +81,10 @@ class ML:
 
     def __modeling(self):
         pass
-    
+
     def __model_evaluation(self):
         pass
-    
+
     def __modelTuning(self):
         pass
 
@@ -62,7 +94,8 @@ class ML:
     def __model_deployment(self):
         pass
 
-def cleanup_column_names(df,rename_dict={},do_inplace=True):
+
+def cleanup_column_names(df, rename_dict={}, do_inplace=True):
     """This function renames columns of a pandas dataframe
        It converts column names to snake case if rename_dict is not passed.
     Args:
@@ -73,8 +106,8 @@ def cleanup_column_names(df,rename_dict={},do_inplace=True):
         pandas dataframe if do_inplace is set to False, None otherwise
     """
     if not rename_dict:
-        return df.rename(columns={col: col.lower().replace(' ','_')
-                    for col in df.columns.values.tolist()},
-                  inplace=do_inplace)
+        return df.rename(columns={col: col.lower().replace(' ', '_')
+                                  for col in df.columns.values.tolist()},
+                         inplace=do_inplace)
     else:
-        return df.rename(columns=rename_dict,inplace=do_inplace)
+        return df.rename(columns=rename_dict, inplace=do_inplace)
